fb8af96231d5279bfe6bf521ac9a8e4bf1e80179
Darel Finkbeiner <finkbeinerd@osti.gov>
2016-01-05 15:55:52 -0500

    add eunit tests

--- src/template_handler.erl
+++ src/template_handler.erl
@@ -16,7 +16,7 @@ handle(Req, State=#state{}) ->
   {ProductBinding, Req3} = cowboy_req:binding(product, Req2),
 	{ok, Body, Req4} = cowboy_req:body(Req3),
 	
-  Output = process_template(ProductBinding, PageBinding, Body),
+  Output = process_template(code:priv_dir(selleck), ProductBinding, PageBinding, Body),
   
 	{ok, Req5} = output(Output, Req4),
 	{ok, Req5, State}.
@@ -24,25 +24,73 @@ handle(Req, State=#state{}) ->
 terminate(_Reason, _Req, _State) ->
 	ok.
   
+output(file_not_found, Req) ->
+  cowboy_req:reply(404, [{<<"content-type">>, <<"text/html">>}], "File Not Found\n", Req);
 output(no_product, Req) ->
-  cowboy_req:reply(404, [{<<"content-type">>, <<"text/html">>}], "Not Such Product\n", Req);
+  cowboy_req:reply(404, [{<<"content-type">>, <<"text/html">>}], "No Such Product\n", Req);
 output(no_template, Req) ->
-  cowboy_req:reply(404, [{<<"content-type">>, <<"text/html">>}], "Not Such Template\n", Req);
+  cowboy_req:reply(404, [{<<"content-type">>, <<"text/html">>}], "No Such Template\n", Req);
 output(Data, Req) ->
   cowboy_req:reply(200, [{<<"content-type">>, <<"text/html">>}], Data, Req).
   
-get_template(Product, TemplateName) ->
-  FileName = code:priv_dir(selleck) ++ "/templates/" ++ binary_to_list(Product) ++ "/" ++ binary_to_list(TemplateName) ++ ".mustache",
-  bbmustache:parse_file(FileName).
+get_template_filename({error, bad_name}, Product, TemplateName) ->
+  get_template_filename("priv/", Product, TemplateName);
+get_template_filename(BaseDir, Product, TemplateName) ->
+  FName = BaseDir ++ "/templates/" ++ binary_to_list(Product) ++ "/" ++ binary_to_list(TemplateName) ++ ".mustache",
+  case file:read_file_info(FName) of
+    {ok, _} -> FName;
+    _ -> file_not_found
+  end.
   
-process_template(undefined, _, _) ->
+get_template(BaseDir, Product, TemplateName) ->
+  FileName = get_template_filename(BaseDir, Product, TemplateName),
+  case FileName of
+    file_not_found -> file_not_found;
+    _ -> bbmustache:parse_file(FileName)
+  end.
+  
+output_template(file_not_found, _) ->
+  file_not_found;
+output_template(Template, Data) ->
+  bbmustache:compile(Template, Data, [{key_type, binary}]).
+  
+process_template(_, undefined, _, _) ->
   no_product;
-process_template(_, undefined, _) ->
+process_template(_, _, undefined, _) ->
   no_template;
-process_template(Product, TemplateName, <<>>) ->
-  OutputTemplate = get_template(Product, TemplateName),
-  bbmustache:compile(OutputTemplate, []);
-process_template(Product, TemplateName, Data) ->
-  OutputTemplate = get_template(Product, TemplateName),
+process_template(BaseDir, Product, TemplateName, <<>>) ->
+  T = get_template(BaseDir, Product, TemplateName),
+  output_template(T, []);
+process_template(BaseDir, Product, TemplateName, Data) ->
   Results = jiffy:decode(Data, [return_maps]),
-  bbmustache:compile(OutputTemplate, Results, [{key_type, binary}]).
+  T = get_template(BaseDir, Product, TemplateName),
+  output_template(T, Results).
+  
+%% Tests
+-ifdef(TEST).
+
+-include_lib("eunit/include/eunit.hrl").
+get_template_test_() ->
+  {"Get Template Tests",
+    [
+      { "File Not Found", ?_assertMatch(file_not_found, get_template("priv/", <<"test">>,<<"bogus_template_name">>))},
+      { "Test File Found", ?_assertMatch({bbmustache, _}, get_template("priv/", <<"test">>,<<"test">>)) }
+    ]
+  }.
+  
+output_template_test_() ->
+  {"Output Template Tests",
+    [{"File Not Found", ?_assertMatch(file_not_found, output_template(file_not_found, {}))}]
+  }.  
+
+process_template_test_() ->
+  {"Process Template Tests",
+    [
+      {"No Product", ?_assertMatch(no_product, process_template("priv/", undefined, <<"test">>, <<>>))},
+      {"No Product, No Template", ?_assertMatch(no_product, process_template("priv/", undefined, undefined, <<>>))},
+      {"No Template", ?_assertMatch(no_template, process_template("priv/", <<"test">>, undefined, <<>>))},
+      {"File Not Found", ?_assertMatch(file_not_found, process_template("priv/", <<"test">>, <<"bogus_template_name">>, <<>>))}
+    ]
+  }.
+
+-endif.
